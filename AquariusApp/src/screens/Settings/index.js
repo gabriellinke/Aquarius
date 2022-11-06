3e78c571b0b044639d26397baa95159a7b2d079c
gabriellinke <gh.linke@hotmail.com>
2022-11-06 03:22:35 -0300

    Salva informações da tela de configurações

--- AquariusApp/src/screens/Settings/index.js
+++ AquariusApp/src/screens/Settings/index.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, ToastAndroid} from 'react-native';
 import DefaultButton from '../../components/DefaultButton';
 import styles from './styles';
 import Input from '../../components/Input';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
   const [dimensions, setDimensions] = useState('');
@@ -26,17 +27,98 @@ const Home = () => {
     ToastAndroid.show('Enviando...', ToastAndroid.SHORT);
   };
 
-  const sendInfo = () => {
-    if (temperature > 20) {
-      sending();
-      setTimeout(() => {
-        showToastSuccess();
-      }, 200);
+  useEffect(() => {
+    async function loadSavedValues() {
+      const savedDimensions = await AsyncStorage.getItem('dimensions');
+      const savedTemperature = await AsyncStorage.getItem('temperature');
+      const savedPh = await AsyncStorage.getItem('ph');
+      const savedStartTime = await AsyncStorage.getItem('startTime');
+      const savedFinishTime = await AsyncStorage.getItem('finishTime');
+
+      if (savedDimensions) {
+        setDimensions(savedDimensions);
+      }
+      if (savedTemperature) {
+        console.log(savedTemperature);
+        setTemperature(savedTemperature);
+      }
+      if (savedPh) {
+        console.log(savedPh);
+        setPh(savedPh);
+      }
+      if (savedStartTime) {
+        setStartTime(savedStartTime);
+      }
+      if (savedFinishTime) {
+        setFinishTime(savedFinishTime);
+      }
+    }
+    loadSavedValues();
+  }, []);
+
+  function inputsAreValid() {
+    return (
+      dimensionsAreValid() &&
+      temperatureIsValid() &&
+      phIsValid() &&
+      timesAreValid()
+    );
+  }
+
+  function dimensionsAreValid() {
+    if (dimensions.length > 0 && dimensions.split('x').length === 3) {
+      return true;
+    }
+    return false;
+  }
+
+  function temperatureIsValid() {
+    if (temperature && temperature >= 20 && temperature <= 50) {
+      return true;
+    }
+    return false;
+  }
+
+  function phIsValid() {
+    if (ph && ph >= 4.5 && ph <= 9) {
+      return true;
+    }
+    return false;
+  }
+
+  function timesAreValid() {
+    if (
+      startTime &&
+      startTime.indexOf(':') > 0 &&
+      finishTime &&
+      finishTime.indexOf(':') > 0
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  function calculateVolume() {
+    const values = dimensions.split('x');
+    return +values[0] * +values[1] * +values[2];
+  }
+
+  const sendInfo = async () => {
+    if (inputsAreValid()) {
+      // POST
+      // if(post.error)
+      // showToastError();
+
+      // Salva as informações no async storage
+      await AsyncStorage.setItem('dimensions', dimensions);
+      await AsyncStorage.setItem('temperature', temperature);
+      await AsyncStorage.setItem('ph', ph);
+      await AsyncStorage.setItem('startTime', startTime);
+      await AsyncStorage.setItem('finishTime', finishTime);
+
+      showToastSuccess();
     } else {
-      sending();
-      setTimeout(() => {
-        showToastError();
-      }, 200);
+      showToastError();
     }
   };
 
@@ -47,7 +129,7 @@ const Home = () => {
           <Input
             inputValue={dimensions}
             setInputValue={setDimensions}
-            label={'Dimensões do aquário'}
+            label={'Dimensões do aquário (cm³)'}
             placeholder={'100x50x20'}
             help={'Exemplo: 50x30x20'}
             mask={'dimension'}
