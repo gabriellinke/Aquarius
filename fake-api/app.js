89df78c50db9afe31986e361369737961ae8c53e
gabriellinke <gh.linke@hotmail.com>
2022-11-10 21:32:50 -0300

    Atualiza fake-api para dar respostas como o ESP32

--- fake-api/app.js
+++ fake-api/app.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+function getRandomFloat(min, max, decimals) {
+  const str = (Math.random() * (max - min) + min).toFixed(decimals);
+
+  return parseFloat(str);
+}
+
+app.get('/', (request, response) => {
+  return response.json({
+    temperatura: getRandomFloat(20, 30, 2),
+    ph: getRandomFloat(5, 9, 2)
+  })
 })
+
+app.post('/update-info', (request, response) => {
+  console.log(request.body);
+  return response.sendStatus(200);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
