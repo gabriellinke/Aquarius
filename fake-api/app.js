fe386f47fb469088fa20164f9de5ad801c470114
gabriellinke <gh.linke@hotmail.com>
2022-11-20 18:10:34 -0300

    Envia notificação utilizando a fake-api

--- fake-api/app.js
+++ fake-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 const port = 3000;
@@ -18,6 +19,17 @@ app.get('/', (request, response) => {
 
 app.post('/update-info', (request, response) => {
   console.log(request.body);
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'key=AAAA9FpWgyU:APA91bFXbera0I3OPrfVGrcXvauVglB6l69VPdqm6Y-uqptf5M22STJngg6GyX5AA0YigqKmam2-WR6pfqtc_iuLf0jprzSpPSf1Gtp9brUC414FB7kJ5FjgniW9SLADS-UGusPThZco'
+  }
+  axios.post('https://fcm.googleapis.com/fcm/send', request.body, {headers: headers})
+    .then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err);
+    })
+  // console.log(request.headers);
   return response.sendStatus(200);
 })
 
