1918080a1d39a61b7552f5aec90a45efe670c744
gabriellinke <gh.linke@hotmail.com>
2022-11-21 00:57:03 -0300

    Adiciona disparo de notificações pelo ESP32 (HTTPS request para o FCM)

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <ESPAsyncWebServer.h>
 #include "AsyncJson.h"
 #include "ArduinoJson.h"
@@ -51,26 +52,61 @@ int horaLigar = 13; // hora que a luz deve ser ligada
 int minutoLigar = 0; // minuto que a luz deve ser ligada
 int horaDesligar = 14; // hora que a luz deve ser desligada
 int minutoDesligar = 5; // minuto que a luz deve ser desligada
+int diaDaSemanaNotificacaoOutrosParametros = 1; // Dia da semana para o envio da notificação de outros parâmetros
+int horaNotificacaoOutrosParametros = 9; // Hora para o envio da notificação de outros parâmetros
+int minutosNotificacaoOutrosParametros = 0; // Minutos para o envio da notificação de outros parâmetros
 float altura = 1; // Altura do aquário em cm
 float largura = 1; // Largura do aquário em cm
 float comprimento = 1; // Comprimento do aquário em cm
-unsigned long timestampValvulas = 4294967295; // Timestamp em que se torna possível fazer uma nova ativação das válvulas
+unsigned long timestampValvulas = 0; // Timestamp em que se torna possível fazer uma nova ativação das válvulas
+unsigned long ultimaNotificacaoPH = 0; // Timestamp da última notificação enviada sobre níveis anormais de pH
+unsigned long ultimaNotificacaoTemperatura = 0; // Timestamp da última notificação enviada sobre valores anormais de temperatura
 unsigned long proximaNotificacaoVerificarOutrosParametros = 4294967295; // Timestamp para o envio da próxima notificação para verificar os outros parâmetros
 unsigned long proximaNotificacaoVerificarReservatorioSolucoesPH = 4294967295; // Timestamp para o envio da próxima notificação para verificar os reservatórios de pH
 unsigned long proximaNotificacaoVerificarReservatorioAgua = 4294967295;  // Timestamp para o envio da próxima notificação para verificar o reservatório de água
-unsigned long ultimaNotificacaoPH = 0; // Timestamp da última notificação enviada sobre níveis anormais de pH
-unsigned long ultimaNotificacaoTemperatura = 0; // Timestamp da última notificação enviada sobre valores anormais de temperatura
-int diaDaSemanaNotificacaoOutrosParametros = 0; // Dia da semana para o envio da notificação de outros parâmetros
-int horaNotificacaoOutrosParametros = 19; // Hora para o envio da notificação de outros parâmetros
-int minutosNotificacaoOutrosParametros = 36; // Minutos para o envio da notificação de outros parâmetros
 
 //==============================================================================================
 // Configurações WiFi, temperatura e tempo
 //==============================================================================================
 /* Configurações WiFi */
 AsyncWebServer server(80);
-const char *ssid = "NET_2GC41029";
-const char *password = "4BC41029";
+const char *ssid = "EGG_2G";
+const char *password = "708234AC";
+
+// Certificado CA obtido via browser
+const char* rootCACertificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFWjCCA0KgAwIBAgIQbkepxUtHDA3sM9CJuRz04TANBgkqhkiG9w0BAQwFADBH\n" \
+"MQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExM\n" \
+"QzEUMBIGA1UEAxMLR1RTIFJvb3QgUjEwHhcNMTYwNjIyMDAwMDAwWhcNMzYwNjIy\n" \
+"MDAwMDAwWjBHMQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNl\n" \
+"cnZpY2VzIExMQzEUMBIGA1UEAxMLR1RTIFJvb3QgUjEwggIiMA0GCSqGSIb3DQEB\n" \
+"AQUAA4ICDwAwggIKAoICAQC2EQKLHuOhd5s73L+UPreVp0A8of2C+X0yBoJx9vaM\n" \
+"f/vo27xqLpeXo4xL+Sv2sfnOhB2x+cWX3u+58qPpvBKJXqeqUqv4IyfLpLGcY9vX\n" \
+"mX7wCl7raKb0xlpHDU0QM+NOsROjyBhsS+z8CZDfnWQpJSMHobTSPS5g4M/SCYe7\n" \
+"zUjwTcLCeoiKu7rPWRnWr4+wB7CeMfGCwcDfLqZtbBkOtdh+JhpFAz2weaSUKK0P\n" \
+"fyblqAj+lug8aJRT7oM6iCsVlgmy4HqMLnXWnOunVmSPlk9orj2XwoSPwLxAwAtc\n" \
+"vfaHszVsrBhQf4TgTM2S0yDpM7xSma8ytSmzJSq0SPly4cpk9+aCEI3oncKKiPo4\n" \
+"Zor8Y/kB+Xj9e1x3+naH+uzfsQ55lVe0vSbv1gHR6xYKu44LtcXFilWr06zqkUsp\n" \
+"zBmkMiVOKvFlRNACzqrOSbTqn3yDsEB750Orp2yjj32JgfpMpf/VjsPOS+C12LOO\n" \
+"Rc92wO1AK/1TD7Cn1TsNsYqiA94xrcx36m97PtbfkSIS5r762DL8EGMUUXLeXdYW\n" \
+"k70paDPvOmbsB4om3xPXV2V4J95eSRQAogB/mqghtqmxlbCluQ0WEdrHbEg8QOB+\n" \
+"DVrNVjzRlwW5y0vtOUucxD/SVRNuJLDWcfr0wbrM7Rv1/oFB2ACYPTrIrnqYNxgF\n" \
+"lQIDAQABo0IwQDAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNV\n" \
+"HQ4EFgQU5K8rJnEaK0gnhS9SZizv8IkTcT4wDQYJKoZIhvcNAQEMBQADggIBADiW\n" \
+"Cu49tJYeX++dnAsznyvgyv3SjgofQXSlfKqE1OXyHuY3UjKcC9FhHb8owbZEKTV1\n" \
+"d5iyfNm9dKyKaOOpMQkpAWBz40d8U6iQSifvS9efk+eCNs6aaAyC58/UEBZvXw6Z\n" \
+"XPYfcX3v73svfuo21pdwCxXu11xWajOl40k4DLh9+42FpLFZXvRq4d2h9mREruZR\n" \
+"gyFmxhE+885H7pwoHyXa/6xmld01D1zvICxi/ZG6qcz8WpyTgYMpl0p8WnK0OdC3\n" \
+"d8t5/Wk6kjftbjhlRn7pYL15iJdfOBL07q9bgsiG1eGZbYwE8na6SfZu6W0eX6Dv\n" \
+"J4J2QPim01hcDyxC2kLGe4g0x8HYRZvBPsVhHdljUEn2NIVq4BjFbkerQUIpm/Zg\n" \
+"DdIx02OYI5NaAIFItO/Nis3Jz5nu2Z6qNuFoS3FJFDYoOj0dzpqPJeaAcWErtXvM\n" \
+"+SUWgeExX6GjfhaknBZqlxi9dnKlC54dNuYvoS++cJEPqOba+MSSQGwlfnuzCdyy\n" \
+"F62ARPBopY+Udf90WuioAnwMCeKpSwughQtiue+hMZL77/ZRBIls6Kl0obsXs7X9\n" \
+"SQ98POyDGCBDTtWTurQ0sR8WNh8M5mQ5Fkzc4P4dyKliPUDqysU0ArSuiYgzNdws\n" \
+"E3PYJ/HQcu51OyLemGhmW/HGY0dVHLqlCFF1pkgl\n" \
+"rqXRfboQnoZsG4q5WTP468SQvvG5\n" \
+"-----END CERTIFICATE-----\n";
 
 // Resposta para quando não for encontrada a rota requisitada
 void notFound(AsyncWebServerRequest *request)
@@ -193,12 +229,11 @@ void atualizaHorario(){
 }
 
 
-// Function that gets current epoch time
+// Função que retorna a timestamp do momento atual
 unsigned long getTimestamp() {
   time_t now;
   return time(&now);
 }
-
 
 // Verifica se está no horário de deixar a luz ligada. Se está, liga a luz e, se não está, desliga a luz
 void verificaLuz(){
@@ -217,6 +252,8 @@ void verificaLuz(){
   }
 }
 
+// Verifica o nível de água. Se está muito baixo, calcula o tempo de ativação da bomba baseado nas 
+// dimensões do aquário e na vazão da bomba, e então liga a bomba por esse tempo
 void verificaNivel(){
   if(nivelAgua < NIVEL_MINIMO){
     medidasComNivelAbaixo++;
@@ -232,6 +269,7 @@ void verificaNivel(){
   }
 }
 
+// Verifica a temperatura. Se ela estiver muito baixa, liga o aquecedor.
 void verificaTemperatura(){
   if(temperatura < temperaturaDesejada - OFFSET_ATIVACAO_TEMPERATURA){
     ligarAquecedor();
@@ -240,6 +278,9 @@ void verificaTemperatura(){
   }
 }
 
+// Verifica se já se passou tempo suficiente desde a última modificação do pH.
+// Verifica se o pH está abaixo. Se estiver, derruba uma gota de solução alcalina.
+// Verifica se o pH está acima. Se estiver, derruba uma gota de solução ácida.
 void verificaPH(){
   unsigned long timestampNow = getTimestamp(); // Pego o timestamp de agora
   if(timestampValvulas < timestampNow){ // Já posso fazer novas alterações no pH?
@@ -253,17 +294,18 @@ void verificaPH(){
   }
 }
 
+// Verifica se é necessário enviar alguma notificação.
 void verificaNotificacao(){
     time_t timestampNow = getTimestamp();
-    printf("Verificando...\n");
+    printf("Verificando necessidade de notificação...\n");
 
     if(timestampNow > ultimaNotificacaoPH + 3600){
         if(ph > phDesejado + OFFSET_NOTIFICACAO_PH){
-            //printf("Dispara notificação pH acima do desejado\n");
+            printf("Dispara notificação pH acima do desejado\n");
             sendPostRequest("Atenção", "O valor do pH está acima do configurado");
             ultimaNotificacaoPH = timestampNow;
         } else if(ph < phDesejado - OFFSET_NOTIFICACAO_PH){
-            //printf("Dispara notificação pH abaixo do desejado\n");
+            printf("Dispara notificação pH abaixo do desejado\n");
             sendPostRequest("Atenção", "O valor do pH está abaixo do configurado");
             ultimaNotificacaoPH = timestampNow;
         }
@@ -271,44 +313,48 @@ void verificaNotificacao(){
 
     if(timestampNow > ultimaNotificacaoTemperatura + 3600){
         if(temperatura > temperaturaDesejada + OFFSET_NOTIFICACAO_TEMPERATURA){
-            //printf("Dispara notificação temperatura acima da desejada\n");
+            printf("Dispara notificação temperatura acima da desejada\n");
             sendPostRequest("Atenção", "O valor da temperatura está acima do configurado");
             ultimaNotificacaoTemperatura = timestampNow;
         } else if(temperatura < temperaturaDesejada - OFFSET_NOTIFICACAO_TEMPERATURA){
-            //printf("Dispara notificação temperatura abaixo da desejada\n");
+            printf("Dispara notificação temperatura abaixo da desejada\n");
             sendPostRequest("Atenção", "O valor da temperatura está abaixo do configurado");
             ultimaNotificacaoTemperatura = timestampNow;
         }
     }
 
     if(timestampNow > proximaNotificacaoVerificarOutrosParametros){
-        //printf("Dispara notificação outros parâmetros\n");
+        printf("Dispara notificação outros parâmetros\n");
         sendPostRequest("Atenção", "Lembre de verificar os outros parâmetros do aquário.");
         proximaNotificacaoVerificarOutrosParametros += 3600*24*7; // Nova notificação daqui a 7 dias
     }
 
     if(timestampNow > proximaNotificacaoVerificarReservatorioAgua){
-        //printf("Dispara notificação reservatório de água\n");
+        printf("Dispara notificação reservatório de água\n");
         sendPostRequest("Atenção", "Lembre de verificar o nível do reservatório de água.");
         proximaNotificacaoVerificarReservatorioAgua += 3600*24; // Nova notificação daqui a 1 dia
     }
 
     if(timestampNow > proximaNotificacaoVerificarReservatorioSolucoesPH){
-        //printf("Dispara notificação reservatório de soluções de pH\n");
+        printf("Dispara notificação reservatório de soluções de pH\n");
         sendPostRequest("Atenção", "Lembre de verificar o nível das soluções reguladoras de pH.");
         proximaNotificacaoVerificarReservatorioSolucoesPH += 3600*24*7; // Nova notificação daqui a 1 dia
     }
 }
 
+// Inicializa as timestamps para as próximas notificações.
 void inicializaTimestampsNotificacoes(){
     printf("\nOutros parâmetros: \n");
-    proximaNotificacaoVerificarOutrosParametros = getNextWeekDayTimestamp(diaDaSemanaNotificacaoOutrosParametros, horaNotificacaoOutrosParametros, minutosNotificacaoOutrosParametros);
+    proximaNotificacaoVerificarOutrosParametros = getNextWeekDayTimestamp(diaDaSemanaNotificacaoOutrosParametros, horaNotificacaoOutrosParametros, minutosNotificacaoOutrosParametros); // Notificação semanal no dia e horário definido pelo usuário
     printf("\n\nReservatorio soluções: \n");
-    proximaNotificacaoVerificarReservatorioSolucoesPH = getNextWeekDayTimestamp(0, 19, 37);
+    proximaNotificacaoVerificarReservatorioSolucoesPH = getNextWeekDayTimestamp(1, 17, 30); // Notificação semanal - Às 17:30 de segunda-feira
     printf("\n\nReservatorio água: \n");
-    proximaNotificacaoVerificarReservatorioAgua = getNextDayTimestamp(19, 38);
+    proximaNotificacaoVerificarReservatorioAgua = getNextDayTimestamp(15, 30); // Notificação diária às 15:30
 }
 
+// Retorna o timestamp da próxima notificação semanal. 
+// A notificação deverá ocorrer no dia da semana e horário passados por parâmetro.
+// Domingo = 0, Sábado = 6
 unsigned long getNextWeekDayTimestamp(int dayOfWeek, int hours, int minutes){
     time_t now;
     time_t timestamp;
@@ -345,6 +391,8 @@ unsigned long getNextWeekDayTimestamp(int dayOfWeek, int hours, int minutes){
     return timestamp;
 }
 
+// Retorna o timestamp da próxima notificação diária. 
+// A notificação deverá ocorrer no horário passado por parâmetro.
 unsigned long getNextDayTimestamp(int hours, int minutes){
     time_t now;
     time_t timestamp;
@@ -378,38 +426,67 @@ unsigned long getNextDayTimestamp(int hours, int minutes){
     return timestamp;
 }
 
-void printLocalTime()
-{
-  atualizaHorario();
-  Serial.print("Horas: ");
-  Serial.print(horaAtual);
-  Serial.print("\tMinutos: ");
-  Serial.print(minutoAtual);
-  Serial.print("\n");
+// Envia um POST request para o servidor do Firebase, que então envia uma notificação
+// com o título e corpo passados como parâmetros da função
+void sendPostRequest(char* title, char* body) {
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    client -> setCACert(rootCACertificate);
+    {
+      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+      HTTPClient https;
+  
+      Serial.print("[HTTPS] begin...\n");
+      if (https.begin(*client, "https://fcm.googleapis.com/fcm/send")) {  // HTTPS
+        Serial.print("[HTTPS] POST...\n");
+        
+        // Add HTTPS headers
+        https.addHeader("Content-Type", "application/json");
+        https.addHeader("Authorization", "key=AAAA9FpWgyU:APA91bFXbera0I3OPrfVGrcXvauVglB6l69VPdqm6Y-uqptf5M22STJngg6GyX5AA0YigqKmam2-WR6pfqtc_iuLf0jprzSpPSf1Gtp9brUC414FB7kJ5FjgniW9SLADS-UGusPThZco");
+
+        // Prepare JSON document
+        DynamicJsonDocument doc(2048);
+        DynamicJsonDocument notification(2048);
+        notification["title"] = title;
+        notification["body"] = body;
+        doc["to"] = "/topics/aquarius";
+        doc["notification"] = notification;
+
+        // Serialize JSON document
+        String json;
+        serializeJson(doc, json);
+                
+        int httpCode = https.POST(json);
+  
+        // httpCode will be negative on error
+        if (httpCode > 0) {
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+  
+          // file found at server
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String payload = https.getString();
+            Serial.println(payload);
+          }
+        } else {
+          Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        }
+  
+        https.end();
+      } else {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
+
+      // End extra scoping block
+    }
+  
+    delete client;
+  } else {
+    Serial.println("Unable to create client");
+  }
 }
 
-void updateInfo(String novoEstadoBomba, String novoEstadoAquecedor, String novoEstadoLuz, int ligarBase, int ligarAcido){
-    if(novoEstadoBomba != NULL && novoEstadoBomba != "null"){
-      estadoBomba = novoEstadoBomba;
-    }
-    if(novoEstadoAquecedor != NULL && novoEstadoAquecedor != "null"){
-      estadoAquecedor = novoEstadoAquecedor;
-    }
-    if(novoEstadoLuz != NULL && novoEstadoLuz != "null"){
-      estadoLuz = novoEstadoLuz;
-    }
-    if(ligarBase != NULL){
-      if(ligarBase == 1){
-        ligarValvulaBase();
-      }
-    }
-    if(ligarAcido != NULL){
-      if(ligarAcido == 1){
-        ligarValvulaAcido();
-      }
-    }
-}
-
+// Inicializa o ESP32 e outros parâmetros
 void setup()
 {
   Serial.begin(115200);
@@ -438,7 +515,7 @@ void setup()
 
   //init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+  atualizaHorario();
 
   inicializaTimestampsNotificacoes();
 
@@ -454,8 +531,6 @@ void setup()
     Serial.println(response);
     request->send(200, "application/json", response);
   });
-
-
 
   AsyncCallbackJsonWebHandler *handler = new AsyncCallbackJsonWebHandler("/update-info", [](AsyncWebServerRequest *request, JsonVariant &json) {
     StaticJsonDocument<500> data;
@@ -489,38 +564,8 @@ void setup()
   server.onNotFound(notFound);
   server.begin();
 }
-
-void sendPostRequest(char* title, char* body) {
-//Check WiFi connection status
-  if(WiFi.status()== WL_CONNECTED){
-    WiFiClient client;
-    HTTPClient http;
-  
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, "http://192.168.0.13:3000/update-info");
     
-    // If you need an HTTP request with a content type: application/json, use the following:
-    http.addHeader("Content-Type", "application/json");
-    http.addHeader("Authorization", "key=AAAA9FpWgyU:APA91bFXbera0I3OPrfVGrcXvauVglB6l69VPdqm6Y-uqptf5M22STJngg6GyX5AA0YigqKmam2-WR6pfqtc_iuLf0jprzSpPSf1Gtp9brUC414FB7kJ5FjgniW9SLADS-UGusPThZco");
-
-    char http_body[200] = "{\"to\":\"/topics/aquarius\",\"notification\": { \"title\":\"";
-    strcat(http_body, title);
-    strcat(http_body, "\",\"body\":\"");
-    strcat(http_body, body);
-    strcat(http_body, "\"}}");
-    Serial.println(http_body);
-
-    int httpResponseCode = http.POST(http_body);
-   
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-      
-    // Free resources
-    http.end();
-  }
-}
- 
-    
+// Loop principal do programa
 void loop()
 {
   atualizaSensores();
